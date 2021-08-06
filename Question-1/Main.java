77cb04ba5ece674927bd34d35892e7fcc00f7ea8
Khalid-accolite <88338165+Khalid-accolite@users.noreply.github.com>
2021-08-06 21:57:33 +0530

    Add files via upload

---
+++ Question-1/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+
+class SecThread implements Runnable{
+    public void run(){
+        System.out.println("Thread using Runnable interface");
+    }
+}
+class FirstThread extends Thread{
+    public void run(){
+        for(int i=1;i<5;i++){
+            try{
+                Thread.sleep(1000);
+                for(int j=0;j<i;j++){
+                    System.out.print("@");
+                }
+                System.out.println();
+            }catch (InterruptedException e){
+                System.out.println("The exception is"+e);
+            }
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        FirstThread ft1 = new FirstThread();
+        FirstThread ft2 = new FirstThread();
+        ft1.start();       //thread created by extending Thread class
+        ft2.start();
+        SecThread st = new SecThread();
+        Thread t = new Thread(st);
+        t.start();                       //thread created by implementing Runnable class
+    }
+}
